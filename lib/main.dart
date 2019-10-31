7df3ae78b0880a281b5eb07229d7c05f9429fde6
Kevin <mrhallonline@gmail.com>
2019-10-31 10:47:47 -0500

    set up scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() {
+return runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue[100] ,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Magic 8 Ball') ,
+            backgroundColor: Colors.blue[800] ,
+          ),
+          body: MagicBall() ,
+        ), 
       ),
     );
+} 
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
